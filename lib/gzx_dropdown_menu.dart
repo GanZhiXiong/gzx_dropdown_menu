27355506a9476a920346a03e1730a87d7a7ab602
GanZhiXiong <ganzhixiong@sina.cn>
2019-06-03 11:28:22 +0800

    first version

---
+++ lib/gzx_dropdown_menu.dart
@@ -0,0 +1,6 @@
+library gzx_dropdown_menu;
+
+export 'src/gzx_dropdown_header.dart';
+export 'src/gzx_dropdown_menu.dart';
+export 'src/gzx_dropdown_menu_controller.dart';
+
---
+++ test/gzx_dropdown_menu_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:gzx_dropdown_menu/gzx_dropdown_menu.dart';
+
+void main() {}
