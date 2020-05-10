8830a07e1c619fa1c890c2ab4e43398d4fe1bc74
GanZhiXiong <1551935335@qq.com>
2020-05-10 21:57:15 +0800

    Add debugShowCheckedModeBanner false

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
